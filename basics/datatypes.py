d34a18c4f7835f52b9f439d83459f48736de9e67
Sakthivel Balasubramaniam <sakthivel.b@thoughtworks.com>
2020-04-26 11:13:58 +0530

    datatypes

---
+++ basics/datatypes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+num = 1810
+decimal = 3.14
+name = "sakthivel"
+
+print(num)
+print(decimal)
+print(name)
+
+print(num+num)
+print(num+decimal)
+
+# Try the exercises below
+# 1. Make a program that displays several numbers.
+print(1, 2, 3, 4, 5)
+# 2. Make a program that solves and shows the summation of 64 + 32.
+print(64 + 32)
+# 3. Do the same as in 2, but make it sum x + y.
+x = 18
+y = 10
+print(x+y)
